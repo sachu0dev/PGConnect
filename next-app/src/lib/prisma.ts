78961d23be8204aa3dadda1d68a426bbfee098a3
sachu0dev <sushil.dev.in@gmail.com>
2024-12-05 14:32:20 +0530

    UP: postgres

--- next-app/src/lib/prisma.ts
+++ next-app/src/lib/prisma.ts
@@ -1,17 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  // @ts-expect-error just ignore this
-  if (!global.prisma) {
-    // @ts-expect-error just ignore this
-    global.prisma = new PrismaClient();
-  }
-  // @ts-expect-error just ignore this
-  prisma = global.prisma;
-}
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
 
-export default prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
